ba3af4980f50c60dbcc66ced9ad2de266ca3303e
Nadia <nadia.mohamed89@gmail.com>
2018-09-11 22:06:36 +0100

    Scaffolding Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'simple#pages/index'
+  resources :articles
+  root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
