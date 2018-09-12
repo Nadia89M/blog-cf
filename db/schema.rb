a792a728afec48b5b6104aa619ff6203a3df73f7
Nadia <nadia.mohamed89@gmail.com>
2018-09-12 18:45:30 +0100

    Content added and Homepage Styled

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_123341) do
+ActiveRecord::Schema.define(version: 2018_09_12_172827) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_09_12_123341) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
   end
 
 end
